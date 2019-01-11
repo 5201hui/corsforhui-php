126aa41ce282cbb752823fa743e51dac7b3e0050
Administrator <dfd@dtdrt>
2019-01-11 09:02:20 +0800

    fgfd

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+//header("Access-Control-Allow-Credentials:true");
+//header('Content-type:text/json');
+//header('Access-Control-Allow-Headers:Content-Type');
+////header('Access-Control-Allow-Origin:http://10.30.29.65:63341');
+//$allow_origin=array(
+//    'http://10.30.29.65:63341',
+//    'http://10.30.29.66:63341'
+//);
+//$origin=isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:'';
+require "header.php";
+$data=file_get_contents("php://input");
+$data=json_decode($data);
+session_start();
+$message=array('message'=>'success');
+
+if(($data->userName==123)&&($data->passWord==123))
+{
+
+    $_SESSION['name']=$data->userName;
+    $_SESSION['password']=$data->passWord;
+     echo json_encode($message);
+}
+else
+{
+    $message['message']='error';
+   echo json_encode($message);
+}
+?>
---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+//header("Access-Control-Allow-Credentials:true");
+//header('Content-type:text/json');
+//header('Access-Control-Allow-Headers:Content-Type');
+////header('Access-Control-Allow-Origin:http://10.30.29.65:63341');
+require "header.php";
+$message=array('message'=>'success');
+echo json_encode($message);
+session_start();
+session_destroy();
+?>
